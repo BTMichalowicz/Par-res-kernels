5e77afc48278a62fb4a27a9d1ee5cd406a67505f
Ben Michalowicz <benjaminmichalowicz98@gmail.com>
2025-11-24 07:12:44 -0800

    PRK Repo for security and other things.

--- SHMEM/Transpose/transpose_sec.c
+++ SHMEM/Transpose/transpose_sec.c
@@ -112,6 +112,7 @@ o The original and transposed matrices are called A and B
 #include <par-res-kern_shmem.h>
 
 #include <math.h>
+#include "shmemx.h"
 
 #define A(i,j)        A_p[(i+istart)+order*(j)]
 #define B(i,j)        B_p[(i+istart)+order*(j)]
@@ -387,7 +388,8 @@ int main(int argc, char ** argv)
       shmem_barrier_all();
 #endif
 
-      shmem_double_put(&Work_in_p[phase-1][0], &Work_out_p[0], Block_size, send_to);
+      //shmem_double_put(&Work_in_p[phase-1][0], &Work_out_p[0], Block_size, send_to);
+      shmemx_secure_put(SHMEM_CTX_DEFAULT, &Work_in_p[phase-1][0], &Work_out_p[0], Block_size, send_to);
       shmem_fence();
 
 #if !BARRIER_SYNCH
